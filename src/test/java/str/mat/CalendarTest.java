4141ff2f0f9e83d6cfa28f166656683bb7c8941d
Mateusz <mateusz.strozik88@gmail.com>
2018-03-04 18:30:49 +0100

    marginal values test

--- src/test/java/str/mat/CalendarTest.java
+++ src/test/java/str/mat/CalendarTest.java
@@ -1,32 +1,57 @@
 package str.mat;
 
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import java.time.LocalDate;
 import java.util.Iterator;
 import str.mat.model.Calendar;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.assertFalse;
 
 public class CalendarTest {
 
-    private Calendar calendar;
+    @Test
+    public void iteratorTest(){
+        Iterator<LocalDate> it = getIterator(19, 30);
 
-    @BeforeTest
-    public void setup(){
-        LocalDate startLocalDate = LocalDate.of(2016, 9,19);
-        LocalDate endLocalDate = LocalDate.of(2016, 9,30);
-        calendar = new Calendar(startLocalDate, endLocalDate);
+        assertEquals(it.next(), getLocalDate(20));
+        assertEquals(it.next(), getLocalDate(23));
+        assertEquals(it.next(), getLocalDate(27));
+        assertEquals(it.next(), getLocalDate(30));
     }
 
     @Test
-    public void calendarIteratorTest(){
-        Iterator<LocalDate> it = calendar.iterator();
+    public void marginalValuesTest(){
+        Iterator<LocalDate> it = getIterator(19, 19);
+        assertFalse(it.hasNext());
 
-        assertEquals(it.next(), LocalDate.of(2016, 9,20));
-        assertEquals(it.next(), LocalDate.of(2016, 9,23));
-        assertEquals(it.next(), LocalDate.of(2016, 9,27));
-        assertEquals(it.next(), LocalDate.of(2016, 9,30));
+        it = getIterator(24, 26);
+        assertFalse(it.hasNext());
 
-        assertEquals(it.hasNext(), false);
+        it = getIterator(20, 20);
+        assertEquals(it.next(), getLocalDate(20));
+        assertFalse(it.hasNext());
+
+        it = getIterator(20, 23);
+        assertEquals(it.next(), getLocalDate(20));
+        assertEquals(it.next(), getLocalDate(23));
+        assertFalse(it.hasNext());
+
+        it = getIterator(19, 24);
+        assertEquals(it.next(), getLocalDate(20));
+        assertEquals(it.next(), getLocalDate(23));
+        assertFalse(it.hasNext());
+    }
+
+    private Iterator<LocalDate> getIterator(int fromDayOfMonth, int toDayOfMonth){
+        LocalDate startLocalDate = LocalDate.of(2016, 9, fromDayOfMonth);
+        LocalDate endLocalDate = LocalDate.of(2016, 9, toDayOfMonth);
+        Calendar calendar = new Calendar(startLocalDate, endLocalDate);
+
+        return calendar.iterator();
+    }
+
+    private LocalDate getLocalDate(int day){
+        return LocalDate.of(2016, 9, day);
     }
 }
